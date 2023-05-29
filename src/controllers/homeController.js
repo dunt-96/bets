396477183c165e41e88ff70fa8f468dad78015b4
DuNT <ntd160296@gmail.com>
2023-05-29 13:42:42 +0700

    #video 25

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,27 +1,33 @@
-import db from '../models/index'
+import db from "../models/index";
+import CRUDService from "../services/CRUDService";
 
 let getHomePage = async (req, res) => {
-       try {
-        let data = await db.User.findAll();
-        return res.render("homepage.ejs", {data: JSON.stringify(data)});
-    } catch (error) {
-        
-    }
-
-    return res.send('data');
-    // return res.send("Hello dunt");
-    // return res.render("homepage.ejs");
-}
+  try {
+    let data = await db.User.findAll();
+    return res.render("homepage.ejs", { data: JSON.stringify(data) });
+  } catch (error) {}
+  return res.send("data");
+};
 let dunt = async (req, res) => {
-    try {
-        let data = await db.User.findAll();
-    } catch (error) {
-        
-    }
-    return res.send("Hello dunt");
-}
+  try {
+    let data = await db.User.findAll();
+  } catch (error) {}
+  return res.send("Hello dunt");
+};
+
+let getCRUD = (req, res) => {
+  res.render("crud.ejs");
+};
+
+let postCRUD = async (req, res) => {
+  let result = await CRUDService.createNewUser(req.body);
+  console.log(result);
+  return res.send("post crud from server");
+};
 
 module.exports = {
-    getHomePage: getHomePage,
-    dunt:dunt,
-}
+  getHomePage: getHomePage,
+  dunt: dunt,
+  getCRUD: getCRUD,
+  postCRUD: postCRUD,
+};
---
+++ src/services/CRUDService.js
@@ -0,0 +1,43 @@
+import bcrypt from "bcrypt";
+import db from "../models/index";
+
+let createNewUser = (data) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let hashPW = await hashUserPassword(data.password);
+      await db.User.create({
+        email: data.email,
+        password: hashPW,
+        firstName: data.firstName,
+        lastName: data.lastName,
+        address: data.address,
+        gender: data.gender === '1' ? true : false,
+        roleId: data.roleId,
+        phoneNumber: data.phoneNumber,
+      });
+
+      resolve('Successful');
+    } catch (error) {
+        reject(error);
+    }
+  });
+};
+
+let hashUserPassword = (password) => {
+  return new Promise(async (resolve, reject) => {
+    const saltRounds = 10;
+
+    try {
+      const salt = await bcrypt.genSaltSync(saltRounds);
+      const hash = await bcrypt.hashSync(password, salt);
+
+      resolve(hash);
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+module.exports = {
+  createNewUser: createNewUser,
+};
